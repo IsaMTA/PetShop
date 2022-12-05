b3c5918809a82a21402414b1ea3e9918688b0db4
Isacc <time.thief4561@gmail.com>
2022-12-05 02:19:41 -0600

    Estableciendo enlace entre proyecto creado en Firebase y proyecto en Angular.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBSzNBiX9kuN9IudcxsRsxsFY1XH1Hahtw',
+    authDomain: 'proyecto-petshop.firebaseapp.com',
+    projectId: 'proyecto-petshop',
+    storageBucket: 'proyecto-petshop.appspot.com',
+    messagingSenderId: '503398519747',
+    appId: '1:503398519747:web:d0040476655dee7762e376',
+    measurementId: 'G-SMEK0ZBN3P'
+  }
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
