6995bc739165552be96112c71f4d8c8001e76573
Isacc <time.thief4561@gmail.com>
2022-12-05 22:26:33 -0600

    Actualizando servicio producto para obtener la lista de Firebase. Métodos para filtrar los productos por categoría fueron añadidos. En cada sección de categoría se despliegan datos reales de la base de datos.

--- src/app/componentes/accesorios-gato/accesorios-gato.component.ts
+++ src/app/componentes/accesorios-gato/accesorios-gato.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { IconoBotonAgregar } from 'src/app/helpers/assets.helper'
+import { ProductService } from 'src/app/servicios/producto.service'
 
 @Component({
   selector: 'app-accesorios-gato',
@@ -9,7 +10,56 @@ import { IconoBotonAgregar } from 'src/app/helpers/assets.helper'
 export class AccesoriosGatoComponent implements OnInit {
   btnAgregar = IconoBotonAgregar
 
-  constructor() {}
+  productos: any[]
 
-  ngOnInit(): void {}
+  constructor(private productoService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productoService
+      .getAllProductos()
+      .subscribe((productos) => (this.productos = productos))
+  }
+
+  get productosFiltrados() {
+    if (this.productos)
+      return this.productoService.getAccesorios('gato', this.productos)
+    return null
+  }
+
+  getImagenProducto(producto: any) {
+    console.log(producto)
+    return producto && producto.img ? producto.img.url : null
+  }
+
+  getPrecio(producto: any) {
+    return producto && producto.precio ? producto.precio.toFixed(2) : null
+  }
+
+  get totalRenglones() {
+    if (this.productosFiltrados) {
+      const total = this.productosFiltrados.length
+      let cantidad = total / 4
+      if (total % 4 != 0) cantidad++
+      return cantidad
+    }
+    return null
+  }
+
+  get listaRenglones() {
+    if (this.totalRenglones && this.productosFiltrados) {
+      let auxLista = [],
+        z = 0
+      for (let i = 0; i < this.totalRenglones; i++) {
+        let auxProductos = []
+        for (let j = 0; j < 4; j++, z++) {
+          if (z < this.productosFiltrados.length) {
+            auxProductos.push(this.productosFiltrados[z])
+          }
+        }
+        auxLista.push(auxProductos)
+      }
+      return auxLista
+    }
+    return null
+  }
 }
--- src/app/servicios/producto.service.ts
+++ src/app/servicios/producto.service.ts
@@ -1,27 +1,74 @@
-import { Injectable } from '@angular/core';
-import { Producto } from '../modelos/ModeloProducto.model';
-import { PRODUCTOS } from '../test/mock-productos';
-import { Observable, of } from 'rxjs';
-import { MessageService } from './mensaje.service';
+import { Injectable } from '@angular/core'
+import { Producto } from '../modelos/ModeloProducto.model'
+import { PRODUCTOS } from '../test/mock-productos'
+import { map, Observable, of } from 'rxjs'
+import { MessageService } from './mensaje.service'
+import { AngularFireDatabase } from '@angular/fire/compat/database'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-
-  constructor(private msgService: MessageService) { }
+  constructor(
+    private msgService: MessageService,
+    private db: AngularFireDatabase
+  ) {}
 
   getProductos(): Observable<Producto[]> {
-    this.msgService.add('ProductService: Productos obtenidos');
-    return of(PRODUCTOS);
+    this.msgService.add('ProductService: Productos obtenidos')
+    return of(PRODUCTOS)
   }
-  
 
   getProducto(id: number): Producto {
-    this.msgService.add(`ProductService: Producto obtenido id=${id}`);
+    this.msgService.add(`ProductService: Producto obtenido id=${id}`)
     for (let i = 0; i < PRODUCTOS.length; i++)
-      if (PRODUCTOS[i].id == id)
-        return PRODUCTOS[i]
-    return PRODUCTOS[0];
+      if (PRODUCTOS[i].id == id) return PRODUCTOS[i]
+    return PRODUCTOS[0]
+  }
+
+  getAllProductos() {
+    return this.db
+      .list('productos')
+      .snapshotChanges()
+      .pipe(
+        map((change) =>
+          change.map((c) => ({
+            key: c.payload.key,
+            ...(c.payload.val() as Record<string, unknown>)
+          }))
+        )
+      )
+  }
+
+  getAlimentos(marca: string, productos: any[]) {
+    let auxProductos: any[] = []
+
+    productos.forEach((producto) => {
+      if (
+        producto.categoria &&
+        producto.categoria.alimento &&
+        producto.categoria.alimento.marca === marca
+      ) {
+        auxProductos.push(producto)
+      }
+    })
+
+    return auxProductos
+  }
+
+  getAccesorios(tipo: string, productos: any[]) {
+    let auxProductos: any[] = []
+
+    productos.forEach((producto) => {
+      if (
+        producto.categoria &&
+        producto.categoria.accesorio &&
+        producto.categoria.accesorio.tipo === tipo
+      ) {
+        auxProductos.push(producto)
+      }
+    })
+
+    return auxProductos
   }
 }
